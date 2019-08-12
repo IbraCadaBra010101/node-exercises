16eca6141f361860f9bb580e8a9e29d05d380295
IbraCadaBra010101 <ibma010101@gmail.com>
2019-08-12 15:29:38 +0200

    nr6

---
+++ modules/data.js
@@ -0,0 +1,5 @@
+module.exports.myArray = ['abc', '123', null, undefined];
+array = [1, 2, 4, 5, 6, 6, 7, 7, 7, 7, 7, 7,];
+module.exports = {
+    numbers: array
+};
---
+++ modules/main.js
@@ -0,0 +1,7 @@
+const data = require('./data');
+const run = () => {
+    data.numbers.forEach(el =>{
+        console.log(el);
+    })
+};
+run();
